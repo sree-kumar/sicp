2c822457669ecb53bf2fad26779c25d4bd119bf0
sreekumar <sreekumar@am.amrita.edu>
2020-05-23 12:17:10 +0530

    edit sqrt.rkt

--- sqrt.rkt
+++ sqrt.rkt
@@ -1,4 +1,17 @@
 #lang racket
+; find the square root of x using 
+; using square root algorithm of Heron of Alexandria.
+; To find an square root of x
+; make a guess of g
+; improve the guess by averaging x and x/g
+; Keep improving the guess until it is good enough.
+; Start g = 1 (Start with 1 as the first guess)
+; Not racket has already (abs x) function
+; so we are using (absolute x) function defined by us
+; Also note that racket provides rational answers 
+; for decimal. so we are using 1.0 and 2.0 for
+; getting decimal values.
+
 (define (absolute x)
   (cond ((< x 0) (- x))
 		((= x 0) 0)
--- squareroot.lisp
+++
@@ -1,7 +0,0 @@
-# find the square root of x using 
-# using square root algorithm of Heron of Alexandria.
-# To find an square root of x
-## make a guess of g
-## improve the guess by averaging x and x/g
-## Keep improving the guess until it is good enough.
-## Start G= 1 (Start with 1 as the first guess)
